18cbd9aa433351c133bd5888f316b6bf12c5c2a9
Muazhere5 <muazctg07@gmail.com>
2025-12-22 02:42:28 +0600

    MongoConnection

--- index.js
+++ index.js
@@ -10,3 +10,29 @@ require("dotenv").config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+/* ============================
+   DATABASE CONNECTION (ATLAS)
+============================ */
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_CLUSTER}/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Project00`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+let db;
+
+async function connectDB() {
+  try {
+    await client.connect();
+    db = client.db(process.env.DB_NAME);
+    console.log("✅ MongoDB Atlas connected successfully");
+  } catch (error) {
+    console.error("❌ MongoDB connection failed:", error);
+  }
+}
+connectDB();
