70632896336c0899cbe59090ca4659f1ecf65297
Muazhere5 <muazctg07@gmail.com>
2025-12-22 03:03:34 +0600

    errorconfig

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const stripe = require("stripe")(process.env.STRIPE_SECRET);
 const { MongoClient, ObjectId, ServerApiVersion } = require("mongodb");
-require("dotenv").config();
+
 /* ============================
    APP CONFIG
 ============================ */
