9dfdebed48872f29e11d1f798f35bafc62ebcf4b
Muazhere5 <muazctg07@gmail.com>
2025-12-22 02:39:23 +0600

    appconfig

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ const jwt = require("jsonwebtoken");
 const stripe = require("stripe")(process.env.STRIPE_SECRET);
 const { MongoClient, ObjectId, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
+/* ============================
+   APP CONFIG
+============================ */
+const app = express();
+app.use(cors());
+app.use(express.json());
